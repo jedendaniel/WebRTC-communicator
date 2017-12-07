97edbfb05d28263ac75c15b280a38fa7d05b39a3
Daniel Koźlik <jedendaniel@gmail.com>
2017-12-07 18:53:15 +0100

    Replace Eclipse for IntelliJ

---
+++ loginUtil/src/main/java/com/webrtcapi/loginUtil/LoginUtilApplication.java
@@ -0,0 +1,12 @@
+package com.webrtcapi.loginUtil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginUtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginUtilApplication.class, args);
+	}
+}
---
+++ loginUtil/src/main/java/com/webrtcapi/model/User.java
@@ -0,0 +1,56 @@
+package com.webrtcapi.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "User")
+public class User {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
