0eaf99e6c39ee71eb5c40e4c912bb2e0b2feac3f
jedendaniel <jedendaniel@gmail.com>
2018-01-02 19:26:40 +0100

    Loading home and chat main page content from html file

---
+++ loginService/src/main/resources/static/chat.js
@@ -0,0 +1,11 @@
+//$(function() {
+////    $("#load-chat").on('click',loadChatMenu());
+//
+//    $("#content-div").html("");
+//    $("#content-div").append("Chat menu text");
+//});
+
+function loadChatMenu(){
+//    $("#content-div").html("");
+    $("#content-div").load("chatMenu.html");
+}
--- loginService/src/main/resources/static/home.js
+++ loginService/src/main/resources/static/home.js
@@ -1,7 +1,12 @@
-function loadHomeContent(){
-    $("#content-div").append("Test text.");
-}
-
 $(function () {
     $(document).ready(function() { loadHomeContent(); });
 });
+
+function loadHomeContent(){
+    $("#content-div").load("home.html");
+}
+
+function logout(){
+    localStorage.removeItem("login");
+    window.location.href = "http://localhost:8090/index.html";
+}
